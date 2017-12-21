23c27f78fbc0744f5b5d068b6aa46c7028d7d54f
Pieter-JanRobrecht <pieter-jan.robrecht@xplore.be>
2017-12-21 10:29:40 +0100

    First test and small fix

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package repositories;
 
 import model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 }
