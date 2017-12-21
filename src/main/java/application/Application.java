04729d75e5bad25407cbfe823d15511449639a28
Pieter-JanRobrecht <pieter-jan.robrecht@xplore.be>
2017-12-21 10:39:18 +0100

    Fix to connect controller

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "java")
+@SpringBootApplication(scanBasePackages = {"controllers", "repositories"})
 public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
